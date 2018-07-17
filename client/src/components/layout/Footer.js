3e8feb30d2ffb28d6a5cd3db9c80fc60c95c353b
puppitty <33668867+puppitty@users.noreply.github.com>
2018-07-16 21:06:54 -0400

    fixed footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -20,12 +20,12 @@ const Footer = () => {
               <ul class="list-inline">
                 <li class="list-inline-item">
                   <a target="_blank" href="https://github.com/ghSB17">
-                    <img src={Github} className="App-logo" alt="Github" height="40px" />
+                    <img src={Github} className="App-logo" alt="Github" style={{ height: "40px", width: "40px" }} />
                   </a>
                 </li>
                 <li class="list-inline-item">
                   <a target="_blank" href="https://www.linkedin.com/in/sumathiganjam/">
-                    <img src={LinkedIn} className="App-logo" alt="LinkedIn" height="40px" />
+                    <img src={LinkedIn} className="App-logo" alt="LinkedIn" style={{ height: "40px", width: "40px" }} />
                   </a>
                 </li>
               </ul>
@@ -41,18 +41,19 @@ const Footer = () => {
               <ul class="list-inline text-center">
                 <li class="list-inline-item px-0">
                   <a target="_blank" href="https://github.com/efratrosmarin">
-                    <img src={Github} className="App-logo" alt="Github" height="40px" />
+                    <img src={Github} className="App-logo" alt="Github" style={{ height: "40px", width: "40px" }} />
                   </a>
                 </li>
                 <li class="list-inline-item px-0 mx-0">
                   <a target="_blank" href="https://www.linkedin.com/in/talefrat/">
-                    <img src={LinkedIn} className="App-logo" alt="LinkedIn" height="40px" />
+                    <img src={LinkedIn} className="App-logo" alt="LinkedIn" style={{ height: "40px", width: "40px" }} />
                   </a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
+
 
         <div className="col-md-4 col-sm-12">
           <div class="card bg-dark text-white">
@@ -63,12 +64,12 @@ const Footer = () => {
               <ul class="list-inline text-center">
                 <li class="list-inline-item">
                   <a target="_blank" href="https://github.com/puppitty">
-                    <img src={Github} className="App-logo" alt="Github" height="40px" />
+                    <img src={Github} className="App-logo" alt="Github" style={{ height: "40px", width: "40px" }} />
                   </a>
                 </li>
                 <li class="list-inline-item">
                   <a target="_blank" href="https://www.linkedin.com/in/joyce-santos-363b084/">
-                    <img src={LinkedIn} className="App-logo" alt="LinkedIn" height="40px" />
+                    <img src={LinkedIn} className="App-logo" alt="LinkedIn" style={{ height: "40px", width: "40px" }} />
                   </a>
                 </li>
               </ul>
